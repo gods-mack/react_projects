801ce7d549fe6b1c5d2f1a6aed4d433c7ea9bc15
gods-mack <manish-1004@outlook.com>
2021-03-21 23:19:16 +0530

    make your own component and use state in fucntional component

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-export default function App() {
+
+const style = StyleSheet.create(
+  {
+    center:{
+      alignItems:'center',
+      tintColor:'red'
+    }
+  }
+)
+
+const Greeting = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.center}>
+      <Text>Hello, {props.name}!</Text>
+    </View>
+  )
+}
+
+
+const YourApp = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+     <Text>Clicked {count} times. </Text>
+     <Button
+        onPress = {() => setCount(count+1)}
+        title ="Click me"
+        />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default YourApp;
