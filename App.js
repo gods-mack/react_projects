3d6230725f3dc6347caf9d242606e379437737fe
gods-mack <manish-1004@outlook.com>
2021-03-27 00:45:50 +0530

    fetch insta api

--- App.js
+++ App.js
@@ -1,37 +1,123 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View, Image,StyleSheet, ScrollView, SafeAreaView,Button } from 'react-native';
+import PostList from './PostList.js';
+import NavigationBar from 'react-native-navbar';
 
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};
+ 
+const titleConfig = {
+  title: 'Helloworld',
+};
+ 
 
-const style = StyleSheet.create(
-  {
-    center:{
-      alignItems:'center',
-      tintColor:'red'
-    }
+var base_url = 'http://insta-apii.herokuapp.com'
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      p_caption: '',
+      p_time:'',
+      p_image:'',
+      posts:[],
+      isLoading: true
+    };
   }
-)
 
-const Greeting = (props) => {
-  return (
-    <View style={style.center}>
-      <Text>Hello, {props.name}!</Text>
-    </View>
-  )
-}
+  componentDidMount() {
+    fetch('https://insta-apii.herokuapp.com/posts/', {method:'GET'})
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({ posts:json});
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+      <View>
+         <NavigationBar
+         title='Instagram'
+         height={50}
+         leftButtonTitle='back'
+         rightButtonTitle='forward'
+      />
+      <ScrollView style={styles.scrollView} >
+     
+        <Text></Text>
+          {
+              this.state.posts.map((p) => {
+                return (
+                  <View style={styles.card}>
+                    <View style={{alignItems:'center', flexDirection:'row'}}>
+                    <Image
+                      style={styles.image}
+                      source={{height:500, width:'97%'}} source={{uri:base_url.concat(p.author.image)}}
+                      resizeMode={"cover"} // <- needs to be "cover" for borderRadius to take effect on Android
+                    />
+                    <View style={{ flexDirection:'column'}}>
+                  <Text style={styles.namestyle_}>{p.author.username}</Text>
+                  <Text style={{paddingLeft:10}}>{p.location}</Text>
+                  </View>
+                  </View>
+                   <Text style={{paddingLeft:5,paddingTop:10,paddingBottom:2}}>{p.caption}</Text>
+                  <Image style={{height:500, width:'97%'}} source={{uri:base_url.concat(p.photo)}} />
+                 
+                  </View>
+                )
+              })
+            }
+      </ScrollView>
+      </View>
+    );
+  }
+};  
 
 
-const YourApp = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-     <Text>Clicked {count} times. </Text>
-     <Button
-        onPress = {() => setCount(count+1)}
-        title ="Click me"
-        />
-    </View>
-  );
-}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding:10,
+    height:'50%'
 
-export default YourApp;
+  },
+  card: {
+    borderWidth: 0,
+    borderRadius: 3,
+    borderColor: '#000',
+    width: '98%',
+    padding: 5
+  },
+  namestyle:{
+    fontWeight:'bold',
+    fontSize:15,
+    padding:10
+  },
+  namestyle_:{
+    fontWeight:'bold',
+    fontSize:20,
+    paddingLeft:10
+  },
+  scrollView:{
+    backgroundColor: 'white',
+    marginHorizontal: 2,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderColor: 'pink',
+    borderWidth: 2,
+    borderRadius: 75
+  },
+ 
+  });
